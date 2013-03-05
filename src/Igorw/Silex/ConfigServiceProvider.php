cbf9ed58dc3b4848a8e9ae0b5021e3dfe6815713
Igor Wiedler <igor@wiedler.ch>
2013-03-05 21:15:28 +0100

    Fix duplicate error message

--- src/Igorw/Silex/ConfigServiceProvider.php
+++ src/Igorw/Silex/ConfigServiceProvider.php
@@ -170,7 +170,6 @@ class ConfigServiceProvider implements ServiceProviderInterface
         $errorMessages = array(
             JSON_ERROR_DEPTH            => 'The maximum stack depth has been exceeded',
             JSON_ERROR_STATE_MISMATCH   => 'Invalid or malformed JSON',
-            JSON_ERROR_STATE_MISMATCH   => 'Control character error, possibly incorrectly encoded',
             JSON_ERROR_CTRL_CHAR        => 'Control character error, possibly incorrectly encoded',
             JSON_ERROR_SYNTAX           => 'Syntax error',
             JSON_ERROR_UTF8             => 'Malformed UTF-8 characters, possibly incorrectly encoded',
