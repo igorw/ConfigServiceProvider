c474d01f0e8512441c8439b07231f88348a1b299
Ruud Denivel <ruud@devolution.be>
2013-06-18 14:34:16 +0200

    yaml support for import resources

--- src/Igorw/Silex/YamlConfigDriver.php
+++ src/Igorw/Silex/YamlConfigDriver.php
@@ -12,11 +12,35 @@ class YamlConfigDriver implements ConfigDriver
             throw new \RuntimeException('Unable to read yaml as the Symfony Yaml Component is not installed.');
         }
         $config = Yaml::parse($filename);
+        $config = $this->loadImports($config, $filename);
+
         return $config ?: array();
     }
 
     public function supports($filename)
     {
         return (bool) preg_match('#\.ya?ml(\.dist)?$#', $filename);
+    }
+
+    public function loadImports($config, $filename)
+    {
+        if (isset($config['imports']) && is_array($config['imports'])) {
+            $path = pathinfo($filename);
+            $imports = $config['imports'];
+            unset($config['imports']);
+
+            $importedConfig = array();
+            foreach ($imports as $import) {
+                $file = $path['dirname'] . '/' . $import['resource'];
+
+                if (is_file($file)) {
+                    $imported = $this->load($file);
+                    $importedConfig = array_merge($importedConfig, $imported);
+                }
+            }
+            $config = array_merge($config, $importedConfig);
+        }
+
+        return $config;
     }
 }
