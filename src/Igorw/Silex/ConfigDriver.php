61cdb01f240c986b341544cc9ae1631ae116f3c0
Rob <rob.bast@gmail.com>
2013-11-21 17:01:58 +0100

    Update ConfigServiceProvider.php
    Could not merge a config key that accepted both a string or array value (default provided was a string, my value was an array). This is my proposed solution. Cannot say with confidence it is elegant or desirable to allow this however. Please close PR if you do not agree or see a valid use case for this behavior.
    
    Also, some CS/PSR2 thingies "fixed".

--- src/Igorw/Silex/ConfigDriver.php
+++ src/Igorw/Silex/ConfigDriver.php
@@ -4,6 +4,6 @@ namespace Igorw\Silex;
 
 interface ConfigDriver
 {
-    function load($filename);
-    function supports($filename);
+    public function load($filename);
+    public function supports($filename);
 }
